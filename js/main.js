0c8a002897adbde35c5795b7df48e1297d5ff444
yusef02 <youssef.bouziane2402@gmail.com>
2024-01-10 16:07:36 +0100

    lavoro completato

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let kmTratta = parseInt(prompt("inserisci i km della tratta: "));
+console.log(kmTratta);
+let etaPasseggero = parseInt(prompt("inserisci l'età del passeggero: "));
+console.log(etaPasseggero);
+
+
+const prezzoPerKm = 0.21;
+let prezzoBiglietto = 0;
+
+if(etaPasseggero < 18){
+    prezzoBiglietto = (kmTratta * prezzoPerKm) / 100 * 80;
+    console.log(prezzoBiglietto);
+} else if(etaPasseggero > 65){
+    prezzoBiglietto = (kmTratta * prezzoPerKm) / 100 * 60;
+    console.log(prezzoBiglietto);
+} else {
+    prezzoBiglietto = (kmTratta * prezzoPerKm);
+    console.log(prezzoBiglietto);
+}
+
+prezzoBiglietto = prezzoBiglietto.toFixed(2);
+const h1Element = document.createElement('h1');
+h1Element.innerHTML = `il costo del tuo biglietto è €${prezzoBiglietto}`;
+document.body.appendChild(h1Element);
